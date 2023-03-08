67411532de9d61a856b22dd3e150f997ab8d0f8b
kaseyuina <tomx2nakaji@gmail.com>
2023-03-08 23:05:10 +0900

    Coding finished, before testing

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,5 @@
 
-
-field = [0,0,0][0,0,0]
+field = [" ", " ", " "], [" ", " ", " "], [" ", " ", " "]
 
 '''
      A   B   C 
@@ -10,6 +9,15 @@ field = [0,0,0][0,0,0]
     ---|---|---
 3      |   |   
 '''
+def show_field():
+    print()
+    print("   A   B   C ")
+    print("1  " + field[0][0] + " | " + field[0][1] + " | " + field[0][2] + " ")
+    print("  ---|---|---")
+    print("2  " + field[1][0] + " | " + field[1][1] + " | " + field[1][2] + " ")
+    print("  ---|---|---")
+    print("3  " + field[2][0] + " | " + field[2][1] + " | " + field[2][2] + " ")
+    print()
 
 turn = True
 completed = False
@@ -24,7 +32,8 @@ while completed == False:
     
     valid = False
     while valid == False:
-        print("Player" + player + "'s turn.")
+        show_field()
+        print("Player" + str(player) + "'s turn.")
         place = input("Please select an empty space. (e.g. A1): ")
         if not len(place) == 2:
             print("Invalid space.")
@@ -41,13 +50,51 @@ while completed == False:
                 elif place[0] == "C":
                     col = 2
                 
-                row = int(place[1] - 1)
+                row = int(place[1]) - 1
 
-                if not field[col][row] == 0:    
-                    print("Not an empty space.")     
+                if not field[row][col] == " ":
+                    print()
+                    print("*******************")
+                    print("Not an empty space!")     
                 else:
                     valid = True
-    field[col][row] = symbol
-    
-                
+    field[row][col] = symbol
+    turn = not turn
 
+    def comp_check():
+        completed = True
+        print()
+        print("Player " + str(player + 1) + " won the game!")
+
+    if field[0][0] == field[0][1] and field[0][1] == field[0][2] and not field[0][0] == " ":
+        comp_check()
+    elif field[1][0] == field[1][1] and field[1][1] == field[1][2] and not field[1][0] == " ":
+        comp_check()
+    elif field[2][0] == field[2][1] and field[2][1] == field[2][2] and not field[2][0] == " ":
+        comp_check()
+    elif field[0][0] == field[1][0] and field[1][0] == field[2][0] and not field[0][0] == " ":
+        comp_check()
+    elif field[0][1] == field[1][1] and field[1][1] == field[2][1] and not field[0][1] == " ":
+        comp_check()
+    elif field[0][2] == field[1][2] and field[1][2] == field[2][2] and not field[0][2] == " ":
+        comp_check()
+    elif field[0][0] == field[1][1] and field[1][1] == field[2][2] and not field[0][0] == " ":
+        comp_check()
+    elif field[0][2] == field[1][1] and field[1][1] == field[2][0] and not field[0][2] == " ":
+        comp_check()
+    
+    draw = True
+    for i in range(3):
+        for j in range(3):
+            if field[i][j] == " ":
+                draw = False
+                break
+        else:
+            continue
+        break
+
+    if draw == True:
+        print()
+        print("******")
+        print("Draw!!")
+        completed = True
